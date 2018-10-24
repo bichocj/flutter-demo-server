8e820ee1f9b222d565dd4ebcf47d24f5a40c42d8
Javier <bichocj@gmail.com>
2018-10-24 07:48:00 -0500

    Update urls.py

--- backend/AppWeb/AppWeb/urls.py
+++ backend/AppWeb/AppWeb/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('accounts/api/', include('rest_auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:  # pragma: no cover
